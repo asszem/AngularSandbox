f7290515600aaa66a6d60562f236467db5307b0a
Andras Olah <andras.antal.olah@hu.ibm.com>
2018-03-29 13:04:28 +0200

    Add Angular Interpolation and Property Binding examples

--- src/app/sandbox/sandbox.component.ts
+++ src/app/sandbox/sandbox.component.ts
@@ -10,26 +10,35 @@ export class SandboxComponent implements OnInit {
   private counter = 0; // let keyword is not allowed to be used inside a class
   private secretCounter: number;
   sandboxPhone: Phone = new Phone();
+  aNumber = 42;
+  imageAddress = 'https://angular.io/assets/images/logos/angular/logo-nav@2x.png';
   constructor() {
     console.log('Constructor called');
   }
 
   ngOnInit() {}
 
-  // Name    return type
-  greet(): string {
-    console.log('Greet() method called: ' + this.counter++);
-    const greetings = 'Hello World!';
-    return greetings;
-  }
-
+  // The return of this function will be displayed in {{displayPhone}} Angular Expression
   displayPhone(): string {
     this.sandboxPhone.setPhoneType('Pixel2');
     // this.sandboxPhone.phoneType = 'alma';
     console.log(
       'Phone type in displayPhone method set to ' + this.sandboxPhone.phoneType
     );
+    this.sandboxPhone.phoneNumber = 123456; // non-private field accessed directly
+    console.log(
+      'Phone number in displayPhone method set to ' +
+        this.sandboxPhone.phoneNumber
+    );
     return this.sandboxPhone.phoneType;
+  }
+
+  setDisabled(status: boolean): boolean {
+    if (status) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 // These statements are OUTSIDE of the SandboxComponent class... not like in Java, here this is possible
@@ -41,11 +50,11 @@ const myAdd = function(x: number, y: number): number {
   return x + y;
 };
 const aPhoneObject: Phone = new Phone();
-let aNumber = 42; // Will not be displayed in the html using {{aNumber}} reference
+let bNumber = 42; // Will not be displayed in the html using {{aNumber}} reference
 
 console.log('myAdd result: ' + myAdd(1, 2));
 console.log('function add result: ' + add(2, 2));
-console.log('A number ' + aNumber + ' incremented to ' + aNumber++);
+console.log('A number ' + bNumber + ' incremented to ' + bNumber++);
 console.log(
   'Phone type: ' +
     aPhoneObject.phoneType +
